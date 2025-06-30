e414368909b786e7c06f33b60d8411eb9db90b73
aDarMar <d.ario27@outlook.it>
2025-06-30 22:09:45 +0200

    Updated dCd0
    * Fixed equivalent wing not updating Sw
    * Added dCd0 for flaps

--- functions/Wing_Design/Equivalent_Wing.m
+++ functions/Wing_Design/Equivalent_Wing.m
@@ -1,4 +1,5 @@
-function [croot_eq,ctip_eq,sweep_eq] = Equivalent_Wing( Sw,ARw,eps_root,eps_tip )
+function [croot_eq,ctip_eq,tr_eq] = Equivalent_Wing( Sw,bw,ctip,...
+    yroot )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,16 +7,16 @@ function [croot_eq,ctip_eq,sweep_eq] = Equivalent_Wing( Sw,ARw,eps_root,eps_tip 
 croot_eq = ((((Sw/(0.5*bw*(1-yroot/bw)))-2*ctip)/(0.5*bw*(1-yroot/bw)))*0.5* ...
     bw*yroot/bw)+(Sw/(0.5*bw*(1-yroot/bw)))-ctip;
 ctip_eq     = ctip;
-xTE_root_eq = xLE_root + croot_eq;
-xTE_tip_eq  = xLE_tip + ctip_eq;
+% xTE_root_eq = xLE_root + croot_eq;
+% xTE_tip_eq  = xLE_tip + ctip_eq;
 tr_eq       = ctip_eq/croot_eq;
 
-sweep_eq    = atan((xLE_tip-xLE_root)/(ytip-yroot))*57.3;
-% sweep_eq_c4 = atan(tand(sweep_eq)-(4/ARw)*(.25*(1-tr_eq)/(1+tr_eq)))*57.3;
-% sweep_eq_c2 = atan(tand(sweep_eq)-(4/ARw)*(.5*(1-tr_eq)/(1+tr_eq)))*57.3;
-
-slop_c      = (ctip_eq-croot_eq)*2/bw;
-slop_tw     = (eps_tip-eps_root)/ytip;
-slop_xle    = (xLE_tip-xLE_root)/ytip;
+% sweep_eq    = atan((xLE_tip-xLE_root)/(ytip-yroot))*57.3;
+% % sweep_eq_c4 = atan(tand(sweep_eq)-(4/ARw)*(.25*(1-tr_eq)/(1+tr_eq)))*57.3;
+% % sweep_eq_c2 = atan(tand(sweep_eq)-(4/ARw)*(.5*(1-tr_eq)/(1+tr_eq)))*57.3;
+% 
+% slop_c      = (ctip_eq-croot_eq)*2/bw;
+% slop_tw     = (eps_tip-eps_root)/ytip;
+% slop_xle    = (xLE_tip-xLE_root)/ytip;
 
 end
